b68ab05bf82c039b9b008e633109be71ae5d7b59
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2021-12-06 07:18:15 +0000

    Improved solution

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -1,37 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Numerics;
+
 const int days = 256;
 
-var fish = (await File.ReadAllTextAsync("Input.txt")).Split(',')
-    .Select(int.Parse)
-    .Select(x => new LanternFish(x))
-    .ToList<ILanterFish>();
+var fishies = (await File.ReadAllTextAsync("Input.txt")).Split(',')
+    .Select(int.Parse);
+
+var arr = new BigInteger[9];
+foreach (var fish in fishies) arr[fish]++;
 
 for (var i = 0; i < days; i++)
 {
-    var length = fish.Count;
-    for (var j = 0; j < length; j++)
+    var newArr = new BigInteger[9];
+    newArr[8] = arr[0];
+    for (var j = 7; j >= 0; j--)
     {
-        switch (fish[j])
+        if (j == 6)
         {
-            case LanternFish { DaysToSpawn: > 0 } x:
-                fish[j] = x with { DaysToSpawn = x.DaysToSpawn - 1 };
-                continue;
-            case LanternFish x:
-                fish[j] = x with { DaysToSpawn = 6 };
-                fish.Add(new ImmatureLanternFish(1));
-                continue;
-            case ImmatureLanternFish { DaysToMaturity: > 0 } x:
-                fish[j] = x with { DaysToMaturity = x.DaysToMaturity - 1 };
-                continue;
-            case ImmatureLanternFish:
-                fish[j] = new LanternFish(6);
-                continue;
+            newArr[6] = arr[7] + arr[0];
+            continue;
         }
+
+        newArr[j] = arr[j + 1];
     }
+
+    arr = newArr;
 }
 
-Console.WriteLine(fish.Count);
-
-internal interface ILanterFish {}
-internal record LanternFish(int DaysToSpawn) : ILanterFish;
-internal record ImmatureLanternFish(int DaysToMaturity) : ILanterFish;
+Console.WriteLine(arr.Aggregate(new BigInteger(), (c, n) => c + n));
