eda31d055e4f8e6dd80a0d983236d6e23bf61008
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2021-12-07 06:54:37 +0000

    Day 7 part 1

---
+++ Day7/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+var submarines = (await File.ReadAllTextAsync("Input.txt")).Split(',').Select(int.Parse).ToList();
+var max = submarines.Max();
+
+var minFuel = int.MaxValue;
+
+for (var i = 0; i <= max; i++)
+{
+    var fuelUsed = 0;
+    foreach (var submarine in submarines)
+    {
+        fuelUsed += Math.Abs(submarine - i);
+    }
+
+    minFuel = Math.Min(minFuel, fuelUsed);
+}
+
+Console.WriteLine(minFuel);
