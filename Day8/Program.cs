45c984a6a2f3103efdf77de6e4392da0489126b0
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2021-12-08 07:05:24 +0000

    Day 8 part 1

---
+++ Day8/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+var inputOutput = (await File.ReadAllLinesAsync("Input.txt")).Select(x => x.Split('|')).ToList();
+var outputs = inputOutput.Select(x => x[1].Split(' ', StringSplitOptions.RemoveEmptyEntries)).ToList();
+
+var uniques = outputs.SelectMany(output => output).Count(num => num.Length is 2 or 3 or 4 or 7);
+
+Console.WriteLine(uniques);
