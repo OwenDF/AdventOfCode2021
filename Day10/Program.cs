d3dc4073d1f337412652d690384f8add908f08bc
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2021-12-11 08:37:11 +0000

    Day 10 part 1

---
+++ Day10/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+var characterRows = (await File.ReadAllLinesAsync("Input.txt")).Select(x => x.ToCharArray()).ToArray();
+var characters = new Dictionary<char, char> { { ')', '(' }, { ']', '[' }, { '}', '{' }, { '>', '<' } };
+
+var syntaxScore = 0;
+
+foreach (var row in characterRows)
+{
+    var stack = new Stack<char>();
+    foreach (var character in row)
+    {
+        if (characters.Values.Contains(character))
+        {
+            stack.Push(character);
+            continue;
+        }
+
+        if (stack.Peek() == characters[character])
+        {
+            stack.Pop();
+            continue;
+        }
+
+        syntaxScore += GetScoreForCharacter(character);
+        break;
+    }
+}
+
+Console.WriteLine(syntaxScore);
+
+int GetScoreForCharacter(char character)
+    => character switch
+    {
+        ')' => 3,
+        ']' => 57,
+        '}' => 1197,
+        '>' => 25137
+    };
