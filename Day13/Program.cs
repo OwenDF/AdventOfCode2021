403a5c122b8bea0cc03fdf20dc07bb075e30fd8b
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2021-12-13 08:07:08 +0000

    Refactor

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -14,20 +14,23 @@ var instructions = lines.Where(x => x.StartsWith("fold along"))
     .Select(x => x.Split("="))
     .Select(ParseOperation);
 
-foreach (var instruction in instructions.Take(1))
+foreach (var instruction in instructions)
 {
     var afterFold = new HashSet<Coordinate>();
     foreach (var coordinate in coordinates)
     {
-        if (instruction.Axis == Axis.X && coordinate.X > instruction.Value)
+        switch (instruction.Axis)
         {
-            afterFold.Add(coordinate with { X = instruction.Value - (coordinate.X - instruction.Value) });
+            case Axis.X when coordinate.X > instruction.Value:
+                afterFold.Add(coordinate with { X = instruction.Value - (coordinate.X - instruction.Value) });
+                break;
+            case Axis.Y when coordinate.Y > instruction.Value:
+                afterFold.Add(coordinate with { Y = instruction.Value - (coordinate.Y - instruction.Value) });
+                break;
+            default:
+                afterFold.Add(coordinate);
+                break;
         }
-        else if (instruction.Axis == Axis.Y && coordinate.Y > instruction.Value)
-        {
-            afterFold.Add(coordinate with { Y = instruction.Value - (coordinate.Y - instruction.Value) });
-        }
-        else afterFold.Add(coordinate);
     }
 
     coordinates = afterFold;
@@ -35,8 +38,24 @@ foreach (var instruction in instructions.Take(1))
 
 Console.WriteLine(coordinates.Count);
 
+var maxX = coordinates.Max(x => x.X);
+var maxY = coordinates.Max(y => y.Y);
+
+var output = new int[maxX + 1, maxY + 1];
+foreach (var coordinate in coordinates) output[coordinate.X, coordinate.Y] = 1;
+
+for (var i = 0; i <= maxY; i++)
+{
+    for (var j = 0; j <= maxX; j++)
+    {
+        Console.Write(output[j, i] == 1 ? "#" : ".");
+    }
+
+    Console.Write(Environment.NewLine);
+}
+
 Operation ParseOperation(string[] arr)
-    => new Operation(
+    => new(
         arr[0] switch
         {
             "x" => Axis.X,
