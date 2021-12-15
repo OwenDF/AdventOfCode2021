837e02d6639e165bc225f4257be9cbbb63c2dd7e
Owen Daly-Furlong <owendalyfurlong@protonmail.com>
2021-12-15 19:55:11 +0000

    Day 15 part 2

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -1,16 +1,49 @@
 ﻿var dangerGrid = (await File.ReadAllLinesAsync("Input.txt"))
     .Select(x => x.ToCharArray().Select(y => int.Parse(y.ToString())).ToArray()).ToArray();
 
-var pathGrid = new int[dangerGrid.Length, dangerGrid[0].Length];
-pathGrid[0, 0] = dangerGrid[0][0];
+var dimensionOne = dangerGrid.Length;
+var dimensionOneBig = dimensionOne * 5;
+var dimensionTwo = dangerGrid[0].Length;
+var dimensionTwoBig = dimensionTwo * 5;
 
-for (var i = 1; i < dangerGrid.Length; i++) pathGrid[i, 0] = pathGrid[i - 1, 0] + dangerGrid[i][0];
-for (var i = 1; i < dangerGrid[0].Length; i++) pathGrid[0, i] = pathGrid[0, i - 1] + dangerGrid[0][i];
-
-for (var i = 1; i < dangerGrid.Length; i++)
-for (var j = 1; j < dangerGrid[0].Length; j++)
+var bigDangerGrid = new int[dimensionOneBig, dimensionTwoBig];
+for (var i = 0; i < dimensionOneBig; i++)
+for (var j = 0; j < dimensionTwoBig; j++)
 {
-    pathGrid[i, j] = dangerGrid[i][j] + Math.Min(pathGrid[i - 1, j], pathGrid[i, j - 1]);
+    var dimOneFactor = i / dimensionOne;
+    var dimTwoFactor = j / dimensionTwo;
+    bigDangerGrid[i, j] = (dangerGrid[i - dimOneFactor * dimensionOne][j - dimTwoFactor * dimensionTwo] + dimOneFactor + dimTwoFactor - 1) % 9 + 1;
 }
 
-Console.WriteLine(pathGrid[dangerGrid.Length - 1, dangerGrid[0].Length - 1] - dangerGrid[0][0]);
+var distances = new int[dimensionOneBig, dimensionTwoBig];
+var unvisitedNodes = new HashSet<(int, int)>();
+var touchedNodes = new HashSet<(int, int)>();
+
+for (var i = 0; i < dimensionOneBig; i++)
+for (var j = 0; j < dimensionTwoBig; j++)
+{
+    distances[i, j] = Int32.MaxValue;
+    unvisitedNodes.Add((i, j));
+}
+
+distances[0, 0] = 0;
+var current = (0, 0);
+unvisitedNodes.Remove((0, 0));
+while (true)
+{
+    foreach (var (x, y) in GetNeighbours(current).Where(x => unvisitedNodes.Contains(x)))
+    {
+        var tentative = distances[current.Item1, current.Item2] + bigDangerGrid[x, y];
+        distances[x, y] = Math.Min(distances[x, y], tentative);
+        touchedNodes.Add((x, y));
+    }
+    
+    unvisitedNodes.Remove(current);
+    if (distances[dimensionOneBig - 1, dimensionTwoBig - 1] < int.MaxValue) break;
+    touchedNodes.IntersectWith(unvisitedNodes);
+    current = touchedNodes.Select(x => (x, distances[x.Item1, x.Item2])).OrderBy(x => x.Item2).First().x;
+}
+
+Console.WriteLine(distances[dimensionOneBig - 1, dimensionTwoBig - 1]);
+
+(int,int)[] GetNeighbours((int i, int j) x) => new[] { (x.i - 1, x.j), (x.i + 1, x.j), (x.i, x.j - 1), (x.i, x.j + 1) };
